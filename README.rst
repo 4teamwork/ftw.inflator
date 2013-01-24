8eaa09e05fbaf9e856ae238999729e3581b62272
Jonas Baumann <jone@jone.ch>
2013-01-24 15:33:31 +0100

    Create package without paster.

---
+++ README.rst
@@ -0,0 +1,31 @@
+ftw.inflator
+============
+
+
+Installation
+------------
+
+- Add ``ftw.inflator`` to your buildout configuration:
+
+::
+
+    [instance]
+    eggs +=
+        ftw.inflator
+
+
+Links
+-----
+
+- Main github project repository: https://github.com/4teamwork/ftw.inflator
+- Issue tracker: https://github.com/4teamwork/ftw.inflator/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.inflator
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.inflator
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.inflator`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.dev0'
+
+
+tests_require = [
+    'unittest2',
+    'plone.app.testing',
+    'ftw.testing',
+    ]
+
+extras_require = {
+    'tests': tests_require,
+    }
+
+setup(name='ftw.inflator',
+      version=version,
+      description='',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw inflator',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.inflator',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
+
+      tests_require=tests_require,
+      extras_require=extras_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
