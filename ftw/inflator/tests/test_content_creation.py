1127da7c1d66497537137b38c8760c22ace1413c
Jonas Baumann <jone@jone.ch>
2013-01-25 10:56:45 +0100

    Creation: reindex objects and make a savepoint.

--- ftw/inflator/tests/test_content_creation.py
+++ ftw/inflator/tests/test_content_creation.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from ftw.inflator.testing import INFLATOR_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PloneSandboxLayer
@@ -41,3 +42,16 @@ class TestContentCreation(TestCase):
         self.assertEqual(
             foo.Title(), 'Foo',
             '/foo has wrong Title "%s" but should be "Foo"' % foo.Title())
+
+    def test_foo_folder_catalog(self):
+        foo_path = '/'.join(self.portal.get('foo').getPhysicalPath())
+        query = {'path': {'query': foo_path,
+                          'depth': 0}}
+
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        brains = catalog.unrestrictedSearchResults(query)
+        self.assertEqual(len(brains), 1)
+
+        foo_brain = brains[0]
+        self.assertEqual(foo_brain.portal_type, 'Folder')
+        self.assertEqual(foo_brain.Title, 'Foo')
